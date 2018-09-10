d8b8581b2dd113b80c000d299cca2f6696d4582d
Kingsley <kingsley.xym@gmail.com>
2018-09-11 02:50:58 +0800

    :sparkles: Add Initial Statistics Funcions With Test Data

--- RecruitAdmin/statistics.php
+++ RecruitAdmin/statistics.php
@@ -9,134 +9,117 @@
 <body>
     <div id="app">
         <v-app>
+            <center>
+                <div class="headline my-3">BBT 18 秋招 · 实时报名数据统计</div>
+            </center>
+
             <v-data-table
-            :headers="headers"
-            :items="desserts"
+            :headers="header"
+            :items="data"
+            :loading="loading"
             hide-actions
             class="elevation-1">
-            <template slot="items" slot-scope="props">
-              <td>{{ props.item.name }}</td>
-              <td>{{ props.item.calories }}</td>
-              <td>{{ props.item.fat }}</td>
-              <td>{{ props.item.carbs }}</td>
-              <td>{{ props.item.protein }}</td>
-              <td>{{ props.item.iron }}</td>
-            </template>
-          </v-data-table>
+                <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.name }}</td>
+                    <td>{{ props.item.all }}</td>
+                    <td>{{ props.item.boy }}</td>
+                    <td>{{ props.item.girl }}</td>
+                    <td>{{ props.item.fcall }}</td>
+                    <td>{{ props.item.fcboy }}</td>
+                    <td>{{ props.item.fcgirl }}</td>
+                    <td>{{ props.item.scall }}</td>
+                    <td>{{ props.item.scboy }}</td>
+                    <td>{{ props.item.scgirl }}</td>
+                </template>
+
+                <template slot="no-data">
+                    <v-alert :value="true" color="error" icon="warning">
+                        唔= = 暂无报名数据
+                    </v-alert>
+                </template>
+            </v-data-table>
+
+            <v-btn medium color="blue" dark @click="toggleDepList">
+                <span v-if="collapsed">收起</span>
+                <span v-else>展开</span>
+                各部门分组
+            </v-btn>
         </v-app>
     </div>
  
     <script src="https://cdn.bootcss.com/vue/2.5.16/vue.min.js"></script>
     <script src="https://cdn.bootcss.com/vuetify/1.1.0-beta.0/vuetify.min.js"></script>
+
     <script>
         new Vue({
             el: '#app',
             data: {
-                headers: [
-                {
-                    text: 'Dessert (100g serving)',
-                    align: 'left',
-                    sortable: false,
-                    value: 'name'
-                },
-                { text: 'Calories', value: 'calories' },
-                { text: 'Fat (g)', value: 'fat' },
-                { text: 'Carbs (g)', value: 'carbs' },
-                { text: 'Protein (g)', value: 'protein' },
-                { text: 'Iron (%)', value: 'iron' }
+                collapsed: false,
+                loading: false,
+                header: [
+                    { text: '名称', value: 'name' },
+                    { text: '报名总人数', value: 'all' },
+                    { text: '男生', value: 'boy' },
+                    { text: '女生', value: 'girl' },
+                    { text: '第一志愿总数', value: 'fcall' },
+                    { text: '第一志愿男生', value: 'fcboy' },
+                    { text: '第一志愿女生', value: 'fcgirl' },
+                    { text: '第二志愿总数', value: 'scall' },
+                    { text: '第二志愿男生', value: 'scboy' },
+                    { text: '第二志愿女生', value: 'scgirl' }
                 ],
-                desserts: [
-                {
-                    value: false,
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: '1%'
-                },
-                {
-                    value: false,
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    iron: '1%'
-                },
-                {
-                    value: false,
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    iron: '7%'
-                },
-                {
-                    value: false,
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    iron: '8%'
-                },
-                {
-                    value: false,
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    iron: '16%'
-                },
-                {
-                    value: false,
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    iron: '0%'
-                },
-                {
-                    value: false,
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    iron: '2%'
-                },
-                {
-                    value: false,
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                    iron: '45%'
-                },
-                {
-                    value: false,
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                    iron: '22%'
-                },
-                {
-                    value: false,
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                    iron: '6%'
+                data: []
+            },
+            methods: {
+                toggleDepList: function () {
+                    this.collapsed = !this.collapsed;
+                    this.loading = true;
+                    if (this.collapsed) {
+                        this.data = [
+                            {
+                                name: '技术部 - 代码组',
+                                all: 1171,
+                                boy: 1983,
+                                girl: 4704,
+                                fcall: 1678,
+                                fcboy: 4700,
+                                fcgirl: 189,
+                                scall: 3415,
+                                scboy: 2164,
+                                scgirl: 922
+                            },
+                            {
+                                name: '技术部 - 设计组',
+                                all: 2643,
+                                boy: 1687,
+                                girl: 490,
+                                fcall: 3424,
+                                fcboy: 1470,
+                                fcgirl: 4203,
+                                scall: 968,
+                                scboy: 1144,
+                                scgirl: 2148
+                            }
+                        ];
+                    } else {
+                        this.data = [
+                            {
+                                name: '技术部',
+                                all: 2825,
+                                boy: 5103,
+                                girl: 888,
+                                fcall: 1263,
+                                fcboy: 4660,
+                                fcgirl: 3401,
+                                scall: 2094,
+                                scboy: 4065,
+                                scgirl: 1616
+                            }
+                        ];
+                    }
+                    this.loading = false;
                 }
-                ]
             }
         })
     </script>
